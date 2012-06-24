ce80fb829fe6807fdf5c94883d14b0b703c22ce8
Brandon Skari <brandon@skari.org>
2012-06-23 18:20:30 -0700

    Show where parsing failed

--- src/parser.cpp
+++ src/parser.cpp
@@ -18,14 +18,22 @@
  * along with SQLassie. If not, see <http://www.gnu.org/licenses/>.
  */
 
+// This needs to be defined prior to including the scanner header
+#define YY_DECL int sql_lex( \
+    ScannerContext* const context, \
+    yyscan_t yyscanner \
+)
+
 #include "AstNode.hpp"
 #include "Logger.hpp"
+#include "nullptr.hpp"
 #include "ParserInterface.hpp"
 #include "QueryRisk.hpp"
+#include "scanner.yy.hpp"
 #include "SensitiveNameChecker.hpp"
 
-#include <iostream>
 #include <fstream>
+#include <ostream>
 #include <string>
 
 using std::cerr;
@@ -34,7 +42,22 @@ using std::cout;
 using std::endl;
 using std::ifstream;
 using std::istream;
+using std::ostream;
 using std::string;
+
+void printParseErrorLocation(const string& query, ostream& out = cout);
+
+// Methods from the parser
+extern void* sqlassieParseAlloc(void* (*allocProc)(size_t numBytes));
+extern void* sqlassieParse(
+    void* parser,
+    int token,
+    const char* identifier,
+    ScannerContext* qrPtr
+);
+extern void* sqlassieParseFree(void* parser, void(*freeProc)(void*));
+// Methods from the scanner
+extern YY_DECL;
 
 /**
  * Parses MySQL queries. Testing code for parser.y
@@ -98,6 +121,9 @@ int main(int argc, char* argv[])
             if (!file)
             {
                 cout << "Invalid: \"" << query << '"' << endl;
+                cout << "Parsing failed near '";
+                printParseErrorLocation(query);
+                cout << '\'' << endl;
             }
             else
             {
@@ -106,4 +132,70 @@ int main(int argc, char* argv[])
         }
     }
     return 0;
+}
+
+void printParseErrorLocation(const string& query, ostream& out)
+{
+    // Try to find where parsing failed
+    QueryRisk qr;
+    ScannerContext sc(&qr);
+    yyscan_t scanner = nullptr;
+    YY_BUFFER_STATE bufferState = nullptr;
+    void* lemonParser = nullptr;
+
+    if (0 != sql_lex_init(&scanner))
+    {
+        goto ERROR;
+    }
+
+    bufferState = sql__scan_string(query.c_str(), scanner);
+    if (nullptr == bufferState)
+    {
+        goto ERROR;
+    }
+
+    lemonParser = sqlassieParseAlloc(malloc);
+    if (nullptr == lemonParser)
+    {
+        goto ERROR;
+    }
+
+    // Keep parsing until we hit an error
+    while (qr.valid)
+    {
+        const int lexToken = sql_lex(&sc, scanner);
+        if (qr.valid)
+        {
+            sqlassieParse(lemonParser, lexToken, nullptr, &sc);
+        }
+    }
+
+    if (!qr.valid)
+    {
+        int lexToken;
+        // We hit an error, so print the reamining tokens
+        do
+        {
+            out << sql_get_text(scanner) << ' ';
+            lexToken = sql_lex(&sc, scanner);
+        }
+        while (0 != lexToken);
+    }
+    goto CLEANUP;
+
+ERROR:
+    cout << " (unknown)";
+CLEANUP:
+    if (nullptr != lemonParser)
+    {
+        sqlassieParseFree(lemonParser, free);
+    }
+    if (nullptr != bufferState)
+    {
+        sql__delete_buffer(bufferState, scanner);
+    }
+    if (nullptr != scanner)
+    {
+        sql_lex_destroy(scanner);
+    }
 }
